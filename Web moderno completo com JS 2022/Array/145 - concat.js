68d9adf5bbc7a7e7f80eccbe8e83973e65dc2272
joaomonteiro7 <114528973+joaomonteiro7@users.noreply.github.com>
2023-09-20 17:30:02 +0100

    changes

---
+++ Web moderno completo com JS 2022/Array/145 - concat.js
@@ -0,0 +1,6 @@
+const filhas = ['abc', 'def']
+const filhos = ['ghi', 'jkl']
+const todos = filhas.concat(filhos)
+console.log(filhas, filhos, todos)
+
+console.log([].concat([1,2], [3,4], 5, [[6,7]]))
---
+++ Web moderno completo com JS 2022/Exercicios/exercicios2/ex 1-5.js
@@ -0,0 +1,49 @@
+console.log("__________________ex1____________________")
+function boasVindas(nome) {
+    return `Ola, ${nome}!`
+}
+
+console.log(boasVindas(`joao`))
+
+console.log("__________________ex2____________________")
+//const idadeDias = idade => idade * 365
+const idadeDias = (idade) =>{
+    return idade * 365
+}
+
+console.log(idadeDias(25))
+
+console.log("__________________ex3____________________")
+const salario = function(horasTrab, valorHora){
+    let result = horasTrab*valorHora;
+    return `Salario igual a ${result} euros`
+}
+
+console.log(salario(150, 40.5))
+
+console.log("__________________ex4____________________")
+const nomeMes = (numMes) =>{
+    switch (numMes) {
+        case 1:
+            return `Janeiro`
+            break;
+        case 2:
+            return `Fevereiro`
+            break;
+        default:
+            return `Mes invalido`
+            break;
+    }
+}
+
+console.log(nomeMes(9))
+
+console.log("__________________ex5____________________")
+const maiorIgual = (num1, num2) =>{
+    let result = (num1 > num2 || num1 == num2)
+    ? true
+    : false
+    return result  
+}
+
+console.log(maiorIgual(5, 1))
