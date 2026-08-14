0f0966f62fb669a426f11a2411b374e76bfbfbeb
João Monteiro <joaomontero07@gmail.com>
2026-08-14 13:00:15 +0100

    225-228

--- Web moderno completo com JS 2022/HTML/exercicios/224-server.js
+++ Web moderno completo com JS 2022/HTML/exercicios/224-server.js
@@ -7,7 +7,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/usuarios", (req, resp) => {
   console.log(req.body);
+  console.log(req.query);
   resp.send("congrats");
+});
+
+app.post("/usuarios/:id", (req, resp) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  resp.send("alterado");
 });
 
 app.listen(3003);
